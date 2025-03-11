1b6887b1f0f9526e50a8dfa8c58b73cac2fec187
Norman Yard <67605918+noyard@users.noreply.github.com>
2025-03-11 13:26:24 -0500

    updated gitignore

--- Create-WingetRepo.ps1
+++ Create-WingetRepo.ps1
@@ -1,1 +1,11 @@
 Create Winget Repo
+$folder = "."
+#$downloadlink = "https://github.com/microsoft/winget-cli-restsource/releases/latest/download/WinGet.RestSource-Winget.PowerShell.Source.zip"
+$downloadlocation = "$folder\WinGet.RestSource-Winget.PowerShell.Source.zip"
+#$download = Invoke-WebRequest -Uri $downloadlink -OutFile $downloadlocation
+
+$unziplocation = "$folder\WinGet.RestSource-Winget.PowerShell.Source"
+#Expand-Archive -Path $downloadlocation -DestinationPath $unziplocation -Force
+#Get-ChildItem -Path $unziplocation -Recurse | Unblock-File
+Import-Module  "$($unziplocation)\Microsoft.WinGet.RestSource.psd1"
+
