eeff2818b8880712fba0031ca7d4edabe7dbb2b5
Norman Yard <67605918+noyard@users.noreply.github.com>
2025-04-08 14:34:49 -0500

    updated get-wingetapp.ps1

--- Get-WingetApp.ps1
+++ Get-WingetApp.ps1
@@ -1,10 +1,11 @@
 function Get-WingetApp {
     param (
         [Parameter(Mandatory=$true)]
-        [string]$AppId,
+        [string]$packageId,
 
-        [Parameter(Mandatory=$true)]
-        [string]$Location
+        [string]$scope = "machine",
+
+        [string]$source = "winget"
     )
 
     # Check if Winget is installed
@@ -13,18 +14,62 @@ function Get-WingetApp {
         return
     }
 
+    # Check if Package Id is valid
+    $result = winget search $packageId
+        if ($result -match "No packages found") {
+        Write-Output "Package ID '$packageId' does not exist."
+        return
+    } else {
+        Write-Output "Package ID '$packageId' exists."
+    }
+
+    #Create temp folder to download to
+    $parent = [System.IO.Path]::GetTempPath()
+    $name = [System.IO.Path]::GetRandomFileName()
+    $Location = Join-Path $parent $name
+    New-Item -ItemType Directory -Path $Location
+
     # Construct the Winget download command
-    $downloadCommand = "winget download --id $AppId --download-directory $Location --Scope machine"
+    $downloadCommand = "winget download --id $($packageId) --download-directory $($location) --Scope $($scope) --Source $($source)"
 
     # Download the app using Winget
     try {
         Invoke-Expression $downloadCommand
-        Write-Output "The app with ID '$AppId' has been successfully downloaded to location '$Location'."
+        Write-Output "The app with ID '$packageId' has been successfully downloaded to location '$Location'."
     }
     catch {
-        Write-Error "An error occurred while trying to download the app with ID '$AppId' to location '$Location'."
+        Write-Error "An error occurred while trying to download the app with ID '$packageId' to location '$Location'."
     }
+
+    $file = Get-ChildItem "$($location)\*.yaml"
+
+    $yamlFile = Join-Path $location $file.name
+    $yamlContent = Get-Content -Path $yamlFile -Raw
+    $parsedData = ConvertFrom-Yaml -Yaml $yamlContent
+    $packageIdentifier = $parseddata.PackageIdentifier
+    $publisher = $packageidentifier.split('.')[0]
+    $packageName = $packageidentifier.split('.')[1]
+    $Version = $parsedData.PackageVersion  
+    $PublisherFirst = $($Publisher.Substring(0, 1)).ToLower()
+    $InstallerUrl = $parseddata.Installers.InstallerUrl
+    $ManifestFolder = "./manifests/$($PublisherFirst)/$($publisher)/$($packageName)/$($Version)"
+
+    if (!(Test-Path $ManifestFolder)) {New-Item -Path $ManifestFolder -ItemType "Directory"}
+
+    #copy yaml to manifest folder
+    Copy-Item -Path $file -Destination $ManifestFolder
+
+    #copy install file to manifest folder
+    $installFile = Get-ChildItem "$($location)\*$([System.IO.Path]::GetExtension($Installerurl))"
+    $destinationFile = "$($ManifestFolder)/$($installerurl |Split-Path -Leaf)"
+    
+    #verify hash of install file
+    Copy-Item -Path $installFile.FullName -Destination $destinationFile
+    $calculatedHash = Get-FileHash -Path $destinationFile -Algorithm SHA256
+    if ($calculatedHash.Hash -eq $parseddata.installers.InstallerSha256) {
+        "The hash matches. The file integrity is verified."
+     } else {
+        "The hash does not match. The file may be corrupted."
+     }
 }
 
-# Example usage:
-# Download-WingetApp -AppId "Microsoft.VisualStudioCode" -Location "C:\Downloads\VSCode"
